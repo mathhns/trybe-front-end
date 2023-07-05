e35393b22cc58c288907165daa2656eb5e42e2f0
Matheus Nunes da Silva <matheusnunessilva17@gmail.com>
2023-07-05 18:09:51 -0300

    progress password generator

--- secao-1/dia1-intro-front-end/src/main.js
+++ secao-1/dia1-intro-front-end/src/main.js
@@ -1,3 +1,6 @@
 import { nanoid } from "nanoid";
 
-console.log(nanoid())
+const passwordBtnEl = document.querySelector('button');
+const displayPasswordEl = document.querySelector('h2');
+
+passwordBtnEl.addEventListener('click', () => displayPasswordEl.innerHTML = nanoid());
