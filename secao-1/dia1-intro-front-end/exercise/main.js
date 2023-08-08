6d7f7f7cdf6a7a1aeb9541f338a1b2c767d8bc16
Matheus Nunes da Silva <matheusnunessilva17@gmail.com>
2023-08-08 16:08:35 -0300

    Alteração via mentoria

--- secao-1/dia1-intro-front-end/exercise/main.js
+++ secao-1/dia1-intro-front-end/exercise/main.js
@@ -1,21 +1,20 @@
-import validator from 'validator';
-import '/style.css'
+import validator from "validator";
+import "/style.css";
 
-const campoDeTexto = document.querySelector('#value');
-const button = document.querySelector('#button');
-const seletor = document.querySelector('#option');
-const textoDeSaida = document.querySelector('#answer');
+const campoDeTexto = document.querySelector("#value");
+const button = document.querySelector("#button");
+const seletor = document.querySelector("#option");
+const textoDeSaida = document.querySelector("#answer");
 
-const UUID_VERSION = 4
+const UUID_VERSION = 4;
 
-button.addEventListener('click', (event) => {
+button.addEventListener("click", (event) => {
   event.preventDefault();
 
   const campos = {
-    cpf: validator.isTaxID(campoDeTexto.value, 'pt-BR'),
+    cpf: validator.isTaxID(campoDeTexto.value, "pt-BR"),
     hexColor: validator.isHexColor(campoDeTexto.value),
     email: validator.isEmail(campoDeTexto.value),
-    uuid: validator.isUUID(campoDeTexto.value, UUID_VERSION),
     uuid: validator.isUUID(campoDeTexto.value, 4),
     url: validator.isURL(campoDeTexto.value),
   };
