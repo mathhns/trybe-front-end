4261f36abef5626edbfbb75a85f34c3d3d9df426
Matheus Nunes da Silva <matheusnunessilva17@gmail.com>
2023-08-09 17:07:53 -0300

    Instalando linter da Trybe

--- secao-1/dia1-intro-front-end/src/main.js
+++ secao-1/dia1-intro-front-end/src/main.js
@@ -1,16 +1,17 @@
-import { nanoid } from "nanoid";
-import "./style.css";
-import copy from "clipboard-copy";
+import { nanoid } from 'nanoid';
+import './style.css';
+import copy from 'clipboard-copy';
 
-const passwordBtnEl = document.querySelector("button");
-const displayPasswordEl = document.querySelector("h2");
+const passwordBtnEl = document.querySelector('button');
+const displayPasswordEl = document.querySelector('h2');
 
-passwordBtnEl.addEventListener("click", () => {
+passwordBtnEl.addEventListener('click', () => {
   const randomPassword = nanoid();
   displayPasswordEl.innerHTML = randomPassword;
 });
 
-displayPasswordEl.addEventListener("click", (event) => {
+displayPasswordEl.addEventListener('click', (event) => {
   copy(event.target.innerHTML);
-  alert("Senha copiada!");
+  // eslint-disable-next-line no-alert
+  alert('Senha copiada!');
 });
